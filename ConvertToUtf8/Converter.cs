a6f0f1e360a0e733896068638aa4cbaf5a15dd27
Paul Rohorzka <paul@paulroho.com>
2016-03-04 00:10:34 +0100

    Extracted conversion to Converter class

---
+++ ConvertToUtf8/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ConvertToUtf8
+{
+    class Converter
+    {
+        public void Convert(string inputFile, string outputFile)
+        {
+            using (var reader = new StreamReader(inputFile, Encoding.Unicode))
+            {
+                using (var writer = new StreamWriter(outputFile, false, Encoding.UTF8))
+                {
+                    CopyContents(reader, writer);
+                }
+            }
+        }
+
+        private void CopyContents(TextReader input, TextWriter output)
+        {
+            var buffer = new char[8192];
+            int len;
+            while ((len = input.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                output.Write(buffer, 0, len);
+            }
+        }
+    }
+}
--- ConvertToUtf8/Program.cs
+++ ConvertToUtf8/Program.cs
@@ -1,31 +1,14 @@
-﻿using System.IO;
-using System.Text;
-
-namespace ConvertToUtf8
+﻿namespace ConvertToUtf8
 {
-    static class FileConverter
+    static class Program
     {
         static void Main(string[] args)
         {
             var inputFile = args[0];
             var outputFile = args[1];
-            using (var reader = new StreamReader(inputFile, Encoding.Unicode))
-            {
-                using (var writer = new StreamWriter(outputFile, false, Encoding.UTF8))
-                {
-                    CopyContents(reader, writer);
-                }
-            }
-        }
 
-        static void CopyContents(TextReader input, TextWriter output)
-        {
-            var buffer = new char[8192];
-            int len;
-            while ((len = input.Read(buffer, 0, buffer.Length)) != 0)
-            {
-                output.Write(buffer, 0, len);
-            }
+            var converter = new Converter();
+            converter.Convert(inputFile, outputFile);
         }
     }
 }

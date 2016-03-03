385ca9c47ba0f307edb236964734b4a7fa39ed9d
Paul Rohorzka <paul@paulroho.com>
2016-03-03 23:51:52 +0100

    Initial commit (code from http://stackoverflow.com/a/265444/571213)

---
+++ ConvertToUtf8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class FileConverter
+{
+    static void Main(string[] args)
+    {
+        string inputFile = args[0];
+        string outputFile = args[1];
+        using (StreamReader reader = new StreamReader(inputFile, Encoding.Unicode))
+        {
+            using (StreamWriter writer = new StreamWriter(outputFile, false, Encoding.UTF8))
+            {
+                CopyContents(reader, writer);
+            }
+        }
+    }
+
+    static void CopyContents(TextReader input, TextWriter output)
+    {
+        char[] buffer = new char[8192];
+        int len;
+        while ((len = input.Read(buffer, 0, buffer.Length)) != 0)
+        {
+            output.Write(buffer, 0, len);
+        }
+    }
+}

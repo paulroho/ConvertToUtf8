18c09a3de345dfa251f26d8cc680a56615619251
Paul Rohorzka <paul@paulroho.com>
2016-03-03 23:59:41 +0100

    Minor cleanup

--- ConvertToUtf8/Program.cs
+++ ConvertToUtf8/Program.cs
@@ -1,29 +1,31 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Text;
 
-class FileConverter
+namespace ConvertToUtf8
 {
-    static void Main(string[] args)
+    static class FileConverter
     {
-        string inputFile = args[0];
-        string outputFile = args[1];
-        using (StreamReader reader = new StreamReader(inputFile, Encoding.Unicode))
+        static void Main(string[] args)
         {
-            using (StreamWriter writer = new StreamWriter(outputFile, false, Encoding.UTF8))
+            var inputFile = args[0];
+            var outputFile = args[1];
+            using (var reader = new StreamReader(inputFile, Encoding.Unicode))
             {
-                CopyContents(reader, writer);
+                using (var writer = new StreamWriter(outputFile, false, Encoding.UTF8))
+                {
+                    CopyContents(reader, writer);
+                }
             }
         }
-    }
 
-    static void CopyContents(TextReader input, TextWriter output)
-    {
-        char[] buffer = new char[8192];
-        int len;
-        while ((len = input.Read(buffer, 0, buffer.Length)) != 0)
+        static void CopyContents(TextReader input, TextWriter output)
         {
-            output.Write(buffer, 0, len);
+            var buffer = new char[8192];
+            int len;
+            while ((len = input.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                output.Write(buffer, 0, len);
+            }
         }
     }
 }
